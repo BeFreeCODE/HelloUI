ae06f8a49d47769998c8c3449e55110aff54e320
BeFreeCODE <kihanll@naver.com>
2013-04-02 14:35:18 +0900

    First Start SingleView

---
+++ HelloUI/CAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CAppDelegate.h
+//  HelloUI
+//
+//  Created by hoseogame15 on 13. 4. 2..
+//  Copyright (c) 2013년 TeamCreator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
